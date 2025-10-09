2e649565e1aeb7ca49f46aa243a7cc64ee33b20e
Sandaru Bandara <124494609+SandaruBandara2001@users.noreply.github.com>
2025-10-09 22:46:39 +0530

    gold css

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,63 +1,60 @@
-import React from 'react';
-import Slider from 'react-slick';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-// Import images using correct RELATIVE paths
-import twitterImg from '../assets/images/twitter.png';
-import facebookImg from '../assets/images/facebook.png';
-import instagramImg from '../assets/images/instagram.png';
-import linkedinImg from '../assets/images/linkedin.png';
-import githubImg from '../assets/images/github.png';
+import twitterImg from "../assets/images/twitter.png";
+import facebookImg from "../assets/images/facebook.png";
+import instagramImg from "../assets/images/instagram.png";
+import linkedinImg from "../assets/images/linkedin.png";
+import githubImg from "../assets/images/github.png";
 
 const Socials = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 947,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 690,
-                settings: {
-                    slidesToShow: 1,
-                }
-            }
-        ]
-    };
+  const settings = {
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 450,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    responsive: [
+      { breakpoint: 947, settings: { slidesToShow: 2 } },
+      { breakpoint: 690, settings: { slidesToShow: 1 } },
+    ],
+  };
 
-    const socialLinks = [
-        { name: 'Twitter', img: twitterImg, url: 'https://twitter.com/Sandaru18769360' },
-        { name: 'Facebook', img: facebookImg, url: 'https://www.facebook.com/sandaru.bandara27?mibextid=LQQJ4d' },
-        { name: 'Instagram', img: instagramImg, url: 'https://instagram.com/zandaru._?igshid=MzRlODBiNWFlZA==' },
-        { name: 'LinkedIn', img: linkedinImg, url: 'https://www.linkedin.com/in/sandaru-bandara-5ab147220/' },
-        { name: 'GitHub', img: githubImg, url: 'https://github.com/SandaruBandara2001' },
-    ];
+  const socialLinks = [
+    { name: "Twitter",   img: twitterImg,   url: "https://twitter.com/Sandaru18769360" },
+    { name: "Facebook",  img: facebookImg,  url: "https://www.facebook.com/sandaru.bandara27?mibextid=LQQJ4d" },
+    { name: "Instagram", img: instagramImg, url: "https://instagram.com/zandaru._?igshid=MzRlODBiNWFlZA==" },
+    { name: "LinkedIn",  img: linkedinImg,  url: "https://www.linkedin.com/in/sandaru-bandara-5ab147220/" },
+    { name: "GitHub",    img: githubImg,    url: "https://github.com/SandaruBandara2001" },
+  ];
 
-    return (
-        <section className="teams" id="teams">
-            <div className="max-width">
-                <h2 className="title">Social Media</h2>
-                <Slider {...settings}>
-                    {socialLinks.map(social => (
-                        <div className="card" key={social.name}>
-                            <div className="box">
-                                <a href={social.url} target="_blank" rel="noopener noreferrer">
-                                    <img src={social.img} alt={social.name} />
-                                </a>
-                                <div className="text">{social.name}</div>
-                            </div>
-                        </div>
-                    ))}
-                </Slider>
+  return (
+    <section className="teams" id="teams">
+      <div className="max-width">
+        <h2 className="title">Social Media</h2>
+        <Slider {...settings} className="socials-slider">
+          {socialLinks.map((s) => (
+            <div className="social-slide" key={s.name}>
+              <div className="card">
+                <div className="box">
+                  <a href={s.url} target="_blank" rel="noopener noreferrer" className="social-link">
+                    <img src={s.img} alt={s.name} className="social-img" />
+                    <div className="text">{s.name}</div>
+                  </a>
+                </div>
+              </div>
             </div>
-        </section>
-    );
-}
+          ))}
+        </Slider>
+      </div>
+    </section>
+  );
+};
 
 export default Socials;
