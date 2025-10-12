00aca410e4f384024ecca4ce8c693fb87232948c
Sandaru Bandara <124494609+SandaruBandara2001@users.noreply.github.com>
2025-10-12 20:53:21 +0530

    vc 1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
+// src/components/About.jsx
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
-import portfolioImage from '../assets/images/Portfolio01.JPG'; // Make sure this path is correct
 
 const About = () => {
   return (
@@ -9,26 +9,19 @@ const About = () => {
         <h2 className="title">About me</h2>
         <div className="about-content">
           <div className="column left">
-            <img src={portfolioImage} alt="Sandaru Bandara" />
+            <img src="/images/Portfolio01.jpg" alt="Sandaru Bandara" />
           </div>
           <div className="column right">
             <div className="text">
               I'm Sandaru and I'm a{' '}
               <TypeAnimation
-                sequence={[
-                  'Developer',
-                  1000,
-                  'Student',
-                  1000,
-                  'Designer',
-                  1000,
-                ]}
+                sequence={['Developer',1000,'Student',1000,'Designer',1000]}
                 wrapper="span"
                 speed={50}
                 repeat={Infinity}
               />
             </div>
-            <p>I’m Sandaru Bandara 21 years old, a software engineering undergraduate student in university of westminster. As an undergraduate, I am an energetic and passionate individual who approaches tasks and situations with a mature and responsible mindset. I consistently strive for excellence and aim to achieve the highest standards in everything I undertake. I am experienced in working in challenging and fast-paced environments, effectively managing multiple projects and priorities simultaneously.</p>
+            <p>…</p>
             <a href="https://drive.google.com/file/d/1Zb4H-07vy0bkUa3ZtK1OBCO999-xKEKd/view?usp=sharing" target="_blank" rel="noopener noreferrer">Download CV</a>
           </div>
         </div>
