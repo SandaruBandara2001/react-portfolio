cbeb14d9965d8417821dfe54e179abf2627beee4
Sandaru Bandara <124494609+SandaruBandara2001@users.noreply.github.com>
2025-10-12 21:07:18 +0530

    vc3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <h2 className="title">About me</h2>
         <div className="about-content">
           <div className="column left">
-            <img src="src/assets/images/Portfolio01.JPG" alt="Sandaru Bandara" />
+            <img src="/images/Portfolio01.JPG" alt="Sandaru Bandara" />
           </div>
           <div className="column right">
             <div className="text">
@@ -21,7 +21,7 @@ const About = () => {
                 repeat={Infinity}
               />
             </div>
-            <p></p>
+            <p>I’m Sandaru Bandara 21 years old, a software engineering undergraduate student in university of westminster. As an undergraduate, I am an energetic and passionate individual who approaches tasks and situations with a mature and responsible mindset. I consistently strive for excellence and aim to achieve the highest standards in everything I undertake. I am experienced in working in challenging and fast-paced environments, effectively managing multiple projects and priorities simultaneously.</p>
             <a href="https://drive.google.com/file/d/1Zb4H-07vy0bkUa3ZtK1OBCO999-xKEKd/view?usp=sharing" target="_blank" rel="noopener noreferrer">Download CV</a>
           </div>
         </div>
